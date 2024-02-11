aba1d9f865f683931ea576b53bf30404d0208316
HarrisLich <112398044+HarrisLich@users.noreply.github.com>
2024-02-11 10:20:47 -0500

    test

--- src/components/Form.js
+++ src/components/Form.js
@@ -118,7 +118,7 @@ export default function Form() {
                             <input onChange={(e) => setRequests(e.target.value)} id="requests" placeholder='Pennsylvania' className="rounded outline outline-1 focus:outline-blue-500 focus:outline-2 mt-2 px-2 md:py-1"></input>
                         </div>
                     </div>
-                    <div className="flex flex-row md:mt-6 mt-2">
+                    <div className="flex flex-row md:mt-6 mt-1">
                         <div className="flex flex-col">
                             <label className='font-bold md:text-sm text-xs'>First Name</label>
                             <input onChange={(e) => setFirstName(e.target.value)} id="firstName" placeholder='Chico' className="rounded outline outline-1 focus:outline-blue-500 focus:outline-2 mt-2 px-2 md:py-1"></input>
@@ -129,7 +129,7 @@ export default function Form() {
                         </div>
 
                     </div>
-                    <div className="flex flex-row md:mt-6 mt-2">
+                    <div className="flex flex-row md:mt-6 mt-1">
                         <div className="flex flex-col">
                             <label className='font-bold md:text-sm text-xs'>Date of Birth (DOB)</label>
                             <DatePicker selected={date} onChange={(e) => setDate(e)} className="outline mt-2 outline-1 rounded px-2 md:py-1 focus:outline-2 focus:outline-blue-500" />
@@ -165,7 +165,7 @@ export default function Form() {
                             </select>
                         </div>
                     </div>
-                    <div className="flex flex-row md:mt-6 mt-2">
+                    <div className="flex flex-row md:mt-6 mt-1">
                         <div className="flex flex-col">
                             <label className='font-bold md:text-sm text-xs'>Weight (lbs)</label>
                             <input onChange={(e) => setWeight(e.target.value)} id="weight" type='number' placeholder='150' className="rounded outline outline-1 focus:outline-blue-500 focus:outline-2 mt-2 px-2 md:py-1"></input>
@@ -180,7 +180,7 @@ export default function Form() {
 
 
                     </div>
-                    <div className="flex flex-row md:mt-6 mt-2">
+                    <div className="flex flex-row md:mt-6 mt-1">
                         <div className="flex flex-col">
                             <label className='font-bold md:text-sm text-xs'>Eyes</label>
                             <select onChange={(e) => setEyes(e.target.value)} id="eyes" className="mt-2 outline outline-1 rounded px-2 md:py-1">
@@ -201,7 +201,7 @@ export default function Form() {
                             </select>
                         </div>
                     </div>
-                    <div className="flex flex-row md:mt-6 mt-2">
+                    <div className="flex flex-row md:mt-6 mt-1">
                         <div className="flex flex-col">
                             <label className='font-bold md:text-sm text-xs'>CITY/ZIP OF SELECTED STATE</label>
                             <select onChange={(e) => setCity(e.target.value)} id="city" className="mt-2 w-[15rem] outline outline-1 rounded px-2 md:py-1">
@@ -242,7 +242,7 @@ export default function Form() {
                             </select>
                         </div>
                     </div>
-                    <div className="flex flex-row md:mt-6 mt-2">
+                    <div className="flex flex-row md:mt-6 mt-1">
                         <div className="flex flex-col">
                             <label className='font-bold text-xs'>ID Photo (White back ground, good lighting minimal shadows on face, you want the entirety of your shoulders showing but not too low)</label>
                             <input onChange={async (e) => {
@@ -250,13 +250,13 @@ export default function Form() {
                                 const base64 = await convertToBase64(file)
                                 setIdPhoto(base64)
                             }}
-                                class="mt-2 relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                                class="mt-1 relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
                                 type="file"
                                 id="id" />
 
                         </div>
                     </div>
-                    <div className="flex flex-row md:mt-6 mt-2">
+                    <div className="flex flex-row md:mt-6 mt-1">
                         <div className="flex flex-col">
                             <label className='font-bold text-xs'>SIGNATURE (White paper, use flash make sure there’s no glare, only one signature should be seen, BLACK PEN)</label>
                             <input onChange={async (e) => {
@@ -264,14 +264,14 @@ export default function Form() {
                                 const base64 = await convertToBase64(file)
                                 setSignature(base64)
                             }}
-                                class="mt-2 relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                                class="mt-1 relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
                                 type="file"
                                 id="signature" />
 
                         </div>
                     </div>
                     <div className="w-full flex items-start justify-center">
-                        <button onClick={()=> onSubmit()} className="px-6 py-2 mt-2 hover:bg-green-300/30 transition-all duration-300 text-green-500">Submit</button>
+                        <button onClick={()=> onSubmit()} className="px-6 py-2 hover:bg-green-300/30 transition-all duration-300 text-green-500">Submit</button>
                     </div>
                 </div>
             </div>
