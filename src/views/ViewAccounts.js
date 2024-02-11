5b140b09640ea2fa0c710c694fd28fbec9e3aaff
HarrisLich <112398044+HarrisLich@users.noreply.github.com>
2024-02-11 09:36:11 -0500

    w

--- src/views/ViewAccounts.js
+++ src/views/ViewAccounts.js
@@ -17,17 +17,33 @@ export default function ViewAccount() {
         })
     }, [])
 
-    const removeRefferal = (e, elem, event)=>{
+    const removeRefferal = (e, elem, event) => {
         event.preventDefault()
         axios.delete("https://chicos-backend.onrender.com/api/deleteRefferal", {
             data: {
                 user: elem._id,
                 reffered: e
             }
-        }).then((res)=>{
+        }).then((res) => {
             console.log(res)
         })
         window.location.reload(true)
+    }
+
+    const markAccount = (event, elem) => {
+        event.preventDefault()
+        //mark account
+        if (elem.marked) {
+            //unmark
+            elem.marked = false
+        } else {
+            //mark
+            elem.marked = true
+        }
+        axios.post("https://chicos-backend.onrender.com/api/updateAccount", elem).then((res)=>{
+            window.location.reload(true)
+        })
+        
     }
 
     if (user && user === "chico") {
@@ -36,30 +52,18 @@ export default function ViewAccount() {
                 <a href="/" className="absolute left-1 top-1 text-5xl text-white"><IoIosArrowRoundBack></IoIosArrowRoundBack></a>
                 <div className="w-full flex items-center justify-center text-white font-bold text-4xl mt-[3rem]">Accounts</div>
                 <div className="w-full flex flex-col mt-[4rem] px-4">
-                    {accounts ? accounts.map((elem)=>{
-                        return(
+                    {accounts ? accounts.map((elem) => {
+                        return (
                             <div className="flex py-4 relative text-white flex-col w-full border-b border-1 border-zinc-900">
                                 <div className="font-bold text-xl">{elem.username}</div>
-                                <button onClick={(e)=>{
-                                    e.preventDefault()
-                                    //mark account
-                                    if(elem.marked){
-                                        //unmark
-                                        elem.marked = false
-                                    }else{
-                                        //mark
-                                        elem.marked = true
-                                    }
-                                    axios.post("https://chicos-backend.onrender.com/api/updateAccount", elem)
-                                    window.location.reload(true)
-                                }} className="absolute right-0 top-[50%]">{elem.marked ? <FaCheck color="green"/> : <ImCross color="red"/> }</button>
+                                <button onClick={(event) => markAccount(event, elem)} className="absolute right-0 top-[50%]">{elem.marked ? <FaCheck color="green" /> : <ImCross color="red" />}</button>
                                 <div className="w-full flex-row gap-2">
-                                    <h1 className="flex flex-row gap-2">Refferals ({elem.reffered.length}): 
-                                    {elem.reffered.map((e)=>{
-                                        return(<button onClick={(event)=> removeRefferal(e, elem, event)}>{e}</button>)
-                                    })}
+                                    <h1 className="flex flex-row gap-2">Refferals ({elem.reffered.length}):
+                                        {elem.reffered.map((e) => {
+                                            return (<button onClick={(event) => removeRefferal(e, elem, event)}>{e}</button>)
+                                        })}
                                     </h1>
-                                    
+
                                 </div>
                             </div>
                         )
